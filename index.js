94658cadc93448e9067b4b241b08c06b8933d177
Cassandra <cparisi1290@gmail.com>
2021-04-07 10:28:27 -0400

    modify data layout, and swtich to innerText instead of inner HTML

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
 function fetchRaces() {
     fetch(endPoint)
         .then(resp => resp.json())
-        .then(json => renderRaces(json))
+        .then(renderRaces) //*
 }
 
 function renderRaces(races) {
@@ -18,16 +18,16 @@ function renderRaces(races) {
     races.data.forEach(races => {
 
         const h2 = document.createElement("h2");
-        h2.innerHTML = races.attributes.name; /*tried destructuring but h2 disappeared from broswer*/
+        h2.innerText = races.attributes.name; /*tried destructuring but h2 disappeared from broswer*/
         div.appendChild(h2);
 
-        const city = document.createElement("p");
-        city.innerHTML = races.attributes.city;
-        div.appendChild(city);
+        const cityState = document.createElement("p");
+        cityState.innerText = `${races.attributes.city}, ${races.attributes.state}`
+        div.appendChild(cityState);
 
-        const state = document.createElement("p");
-        state.innerHTML = races.attributes.state;
-        div.appendChild(state);
+        // const state = document.createElement("p");
+        // state.innerHTML = races.attributes.state;
+        // div.appendChild(state);
 
         // const raceDate = document.createElement("p");
         // raceDate.innerHTML = races.attributes.race_date;
@@ -35,19 +35,19 @@ function renderRaces(races) {
 
         
         const distance = document.createElement("p");
-        distance.innerHTML = races.attributes.distance;
+        distance.innerText = `Distance: ${races.attributes.distance}`;
         div.appendChild(distance);
         
         const theme = document.createElement("p");
-        theme.innerHTML = races.attributes.theme.name;
+        theme.innerText = `Theme: ${races.attributes.theme.name}`;
         div.appendChild(theme);
         
         const website = document.createElement("p");
-        website.innerHTML = races.attributes.website;
+        website.innerText = races.attributes.website;
         div.appendChild(website);
         
         const description = document.createElement("p");
-        description.innerHTML = races.attributes.description;
+        description.innerText = races.attributes.description;
         div.appendChild(description);
     })
 }
